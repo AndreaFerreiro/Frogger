da0bbd0d885ee2b73e834ac9e725cf9e0a62926a
“AndreaFerreiro” <andreafga28@gmail.com>
2023-10-18 13:13:09 +0200

    functions to move pieces

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const logsRight = document.querySelectorAll('.log-right');
     const width = 9
     let currentIndex = 76
+    let currentTime = 20
     let timerId
 
     // render frog on starting block
@@ -38,17 +39,161 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // move cars
     function autoMoveCars () {
-        carsLeft.forEach(carLeft => moveCardLeft(carLeft))
-        carsRight.forEach(carRight => moveCardRight(carRight))
+        carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+        carsRight.forEach(carRight => moveCarRight(carRight))
     }
 
     // move the car left on a time loop
-    
+    function moveCarLeft (carLeft) {
+        switch (true){
+            case carLeft.classList.contains('c1'):
+                carLeft.classList.remove('c1')
+                carLeft.classList.add('c2')
+                break
+            case carLeft.classList.contains('c2'):
+                carLeft.classList.remove('c2')
+                carLeft.classList.add('c3')
+                break
+            case carLeft.classList.contains('c3'):
+                carLeft.classList.remove('c3')
+                carLeft.classList.add('c1')
+                break
+        }
+    }
 
+    // move the car righton a time loop
+    function moveCarRight (carRight) {
+        switch (true){
+            case carRight.classList.contains('c1'):
+                carRight.classList.remove('c1')
+                carRight.classList.add('c3')
+                break
+            case carRight.classList.contains('c2'):
+                carRight.classList.remove('c2')
+                carRight.classList.add('c1')
+                break
+            case carRight.classList.contains('c3'):
+                carRight.classList.remove('c3')
+                carRight.classList.add('c2')
+                break
+        }
+    }
 
+    // move the logs
+    function autoMoveLogs() {
+        logsLeft.forEach(logLeft => moveLogleft(logLeft))
+        logsRight.forEach(logRight => moveLogRight(logRight))
+    }
 
+    // move the log left on a time loop
+    function moveLogLeft (logLeft) {
+        switch (true){
+            case logLeft.classList.contains('l1'):
+                logLeft.classList.remove('l1')
+                logLeft.classList.add('l2')
+                break
+            case logLeft.classList.contains('l2'):
+                logLeft.classList.remove('l2')
+                logLeft.classList.add('l3')
+                break
+            case logLeft.classList.contains('l3'):
+                logLeft.classList.remove('l3')
+                logLeft.classList.add('l4')
+                break
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4')
+                logLeft.classList.add('l5')
+                break
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5')
+                logLeft.classList.add('l1')
+                break
+        }
+    }
 
+    // move the log righton a time loop
+    function moveLogRight (logRight) {
+        switch (true){
+            case logRight.classList.contains('l1'):
+                logRight.classList.remove('l1')
+                logRight.classList.add('l5')
+                break
+            case logRight.classList.contains('l2'):
+                logRight.classList.remove('l2')
+                logRight.classList.add('l1')
+                break
+            case logRight.classList.contains('l3'):
+                logRight.classList.remove('l3')
+                logRight.classList.add('l2')
+                break
+            case logRight.classList.contains('l4'):
+                logRight.classList.remove('l4')
+                logRight.classList.add('l3')
+                break
+            case logRight.classList.contains('l5'):
+                logRight.classList.remove('l5')
+                logRight.classList.add('l4')
+                break
+        }
+    }
 
+    // rule to win Frogger
+    if (squares[4].classList.contains('frog')){
+        result.innerHTML = 'You Won!!'
+        squares[currentIndex].classList.remove('frog')
+        clearInterval(timerId)
+        document.removeEventListener('keyup', moveFrog)
+    }
+
+    //rules to move froggers
+    function lose () {
+        if ((currentTime === 0) || (squares[currentIndex].classList.contains('c1')) || (squares[currentIndex].classList.contains('l5')) || (squares[currentIndex].classList.contains('l4'))){
+            result.innerHTML = 'You Lose'
+            squares[currentIndex].classList.remove('frog')
+            clearInterval(timerId)
+            document.removeEventListener('keyup', moveFrog)
+        }
+    }
+
+    // move the frog when its on the log moving left
+    function moveWithLogLeft () {
+        if ( currentIndex >= 27 && currentIndex < 35){
+            squares[currentIndex].classList.remove('frog')
+            currentIndex +=1
+            squares[currentIndex].classList.add('frog')
+        }
+    }
+
+    // move the frog when its on the log moving right
+    function moveWithLogRight () {
+        if ( currentIndex > 18 && currentIndex <= 26){
+            squares[currentIndex].classList.remove('frog')
+            currentIndex -=1
+            squares[currentIndex].classList.add('frog')
+        }
+    }
+
+    // all the functions that move pieces
+    function movePieces () {
+        currentTime --
+        timeLeft.textContent= currentTime
+        autoMoveCars()
+        autoMoveLogs()
+        moveWithLogLeft()
+        moveWithLogRight()
+        lose()
+    }
+
+    // start and pause the game
+    startBtn.addEventListener('click', () =>{
+        if (timerId) {
+            clearInterval(timerId)
+        } else {
+            timerId = setInterval(movePieces, 1000)
+            document.addEventListener('keyup', moveFrog)
+            
+        }
+    })
 
 
 
